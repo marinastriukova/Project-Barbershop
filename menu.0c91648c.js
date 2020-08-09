5d8d42a975ed0c0588a37e93dfeecd6e45935886
Segijua06 <64086212+Segijua06@users.noreply.github.com>
2020-08-10 02:37:16 +0300

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -121,11 +121,13 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 (function () {
   var menuBtnRef = document.querySelector("[data-menu-button]");
   var mobileMenuRef = document.querySelector("[data-menu]");
+  var modalRef = document.querySelector("[data-backdrop]");
   menuBtnRef.addEventListener("click", function () {
     var expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
+    modalRef.classList.toggle("is-hidden");
   });
 })();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -156,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55904" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59085" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
