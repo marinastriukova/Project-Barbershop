fcbc5dafa928c8b4b7737324eb94b24475344394
Segijua06 <64086212+Segijua06@users.noreply.github.com>
2020-08-10 02:36:15 +0300

    Добавил JS
    Добавлено JS для затемнения фона при нажатии кнопки "Меню"

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,10 +7,9 @@
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
     mobileMenuRef.classList.toggle("is-open");
+    modalRef.classList.toggle("is-hidden");
   });
 })();
