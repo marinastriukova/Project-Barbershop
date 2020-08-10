40fc2895e9076eb429ad62e21424c602ad8fd32a
Segijua06 <64086212+Segijua06@users.noreply.github.com>
2020-08-10 04:49:59 +0300

    Добавлены кнопки слайдера
    Добавлены кнопки слайдера, переделаны все кнопки на странице в ссылки. Слайдер добавлен но не настроен

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,12 +4,20 @@
 
   const modalRef = document.querySelector("[data-backdrop]");
 
+  // const refs = {
+  //   closeModalBtn: document.querySelector('[data-modal-close]'),
+  //   };
+
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
     menuBtnRef.classList.toggle("is-open");
+
     menuBtnRef.setAttribute("aria-expanded", !expanded);
+
     mobileMenuRef.classList.toggle("is-open");
+
     modalRef.classList.toggle("is-hidden");
   });
 })();
